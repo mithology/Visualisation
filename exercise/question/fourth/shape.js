072d4b90c9f03014dc15f8d42436036cb177379f
miteshkumarjha <mitesh@thoughtworks.com>
2016-11-15 15:17:51 +0530

    refactoring of code

--- exercise/question/fourth/shape.js
+++ exercise/question/fourth/shape.js
@@ -1,6 +1,6 @@
 var svgContainer = d3.select("div").append("svg")
     .attr("width", 600)
-    .attr("height", 100);
+    .attr("height", 102);
 
 var line = svgContainer.append("line")
     .attr("x1", 110)
@@ -8,19 +8,17 @@ var line = svgContainer.append("line")
     .attr("x2", 10)
     .attr("y2", 100);
 var circle = svgContainer.append("circle")
-    .attr("cx", 220)
-    .attr("cy", 50)
+    .attr("cx", 210)
+    .attr("cy", 51)
     .attr("r", 50);
 
-
 var rectangle = svgContainer.append("rect")
-    .attr("x", 300)
-    .attr("y", 0)
+    .attr("x", 310)
+    .attr("y", 1)
     .attr("rx", 5)
     .attr("ry", 5)
-    .attr("width", 100)
+    .attr("width", 101)
     .attr("height", 100);
-
 var traingle = svgContainer.append("polygon")
     .attr("points", "460,100,460,100,510,0,560,100");
 
