a825c29b43b751a4788081e58eb59464ea6015c1
miteshkumarjha <mitesh@thoughtworks.com>
2016-11-18 20:56:07 +0530

    refactoring:- changed colorScheme from array to D3 colorScheme

--- exercise/exercise17/data.js
+++ exercise/exercise17/data.js
@@ -3,8 +3,7 @@ var width = 800;
 var height = 400;
 var donutWidth = 100;
 var radius = Math.min(width, height) / 2;
-var color = d3.scaleOrdinal()
-    .range(["#1F77B4", "#AEC7E8", "#FF7F0E", "#FFBB78", "#2CA02C", "#98DF8A", "#D62728"]);
+var color = d3.scaleOrdinal(d3.schemeCategory20);
 var loadChart = function () {
     var svg = d3.select('.container')
         .append('svg')
