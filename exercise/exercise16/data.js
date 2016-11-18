2f618fdc8ff8d30ebf35deb1cec75968298bacf6
miteshkumarjha <mitesh@thoughtworks.com>
2016-11-18 18:12:06 +0530

    refactoring :- 1. change the size of the svg. 2.increased the size of Donut radius. 3. Using class instread of id.

--- exercise/exercise16/data.js
+++ exercise/exercise16/data.js
@@ -1,11 +1,11 @@
 var dataSet = [1, 1, 2, 2, 1, 2, 1];
-var width = 1000;
-var height = 600;
-var donutWidth = 75;
+var width = 800;
+var height = 400;
+var donutWidth = 100;
 var radius = Math.min(width, height) / 2;
 var color = d3.scaleOrdinal().range(["#1F77B4", "#AEC7E8", "#FF7F0E", "#FFBB78", "#2CA02C", "#98DF8A", "#D62728"]);
 var loadChart = function () {
-    var svg = d3.select('#chart')
+    var svg = d3.select('.container')
         .append('svg')
         .attr('width', width)
         .attr('height', height)
